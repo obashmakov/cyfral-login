14b1b232d14a95c11d41f2e024535453296116b7
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-12-14 21:06:41 +0200

    added registration for users

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import { Registration } from './components/Registartion';
+import React, { useEffect, useState } from 'react';
+import { Singin } from './components/Singin';
 import './App.scss';
-// import { Switch, Link, Route } from 'react-router-dom';
+import { UserPage } from './components/UserPage';
 
-export const App = () => (
-  <div className="content">
-    <Registration />
-  </div>
-);
+export const App = () => {
+  const [newUser, setNewUser] = useState([]);
+  const [userId, setUserId] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('user') || '[]');
+
+    setNewUser(saved);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(newUser));
+  }, [newUser]);
+
+  useEffect(() => {
+    const find = newUser.find(person => person.id === userId);
+
+    setUser(find);
+  }, [userId]);
+
+  return (
+    <div className="content">
+      {!user && (
+        <Singin
+          setNewUser={setNewUser}
+          newUser={newUser}
+          setUserId={setUserId}
+        />
+      )}
+
+      {user && (
+        <UserPage user={user} />
+      )}
+    </div>
+  );
+};
